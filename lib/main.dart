a683b2866221b9e8451d68483e271d10c875e2a7
Paul <easylive1989@gmail.com>
2022-01-10 23:40:31 +0800

    extract main app to file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
-import 'package:little_flower_app/generated/l10n.dart';
 import 'package:little_flower_app/service/ki_board_service.dart';
-import 'package:little_flower_app/widget/board_info_area.dart';
-import 'package:little_flower_app/widget/ki_board_area.dart';
-import 'package:little_flower_app/widget/result_area.dart';
-import 'package:provider/provider.dart';
 
 import 'injection.dart';
+import 'main_app.dart';
 
 void main() async {
   configureInjection();
@@ -17,57 +12,7 @@ void main() async {
   await Firebase.initializeApp();
   var kiBoardManager = GetIt.I<KiBoardService>();
   runApp(
-    MyApp(kiBoardManager),
+    MainApp(kiBoardManager),
   );
 }
 
-class MyApp extends StatelessWidget {
-  final KiBoardService _kiBoardService;
-
-  MyApp(this._kiBoardService);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: ChangeNotifierProvider(
-          create: (context) => _kiBoardService,
-          child: FutureBuilder(
-              future: _kiBoardService.resetKiBoard(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return _buildKiBoardArea();
-                }
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }),
-        ),
-      ),
-    );
-  }
-}
-
-Widget _buildKiBoardArea() {
-  return Stack(
-    children: [
-      Align(
-        alignment: Alignment.center,
-        child: KiBoardArea(),
-      ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: ResultArea(),
-      ),
-      BoardInfoArea(),
-    ],
-  );
-}
---
+++ lib/main_app.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:little_flower_app/generated/l10n.dart';
+import 'package:little_flower_app/service/ki_board_service.dart';
+import 'package:little_flower_app/widget/board_info_area.dart';
+import 'package:little_flower_app/widget/ki_board_area.dart';
+import 'package:little_flower_app/widget/result_area.dart';
+import 'package:provider/provider.dart';
+
+class MainApp extends StatelessWidget {
+  final KiBoardService _kiBoardService;
+
+  MainApp(this._kiBoardService);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: ChangeNotifierProvider(
+          create: (context) => _kiBoardService,
+          child: FutureBuilder(
+              future: _kiBoardService.resetKiBoard(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return _buildKiBoardArea();
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildKiBoardArea() {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: KiBoardArea(),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: ResultArea(),
+        ),
+        BoardInfoArea(),
+      ],
+    );
+  }
+}
