170d61f6c36a54315c7aede991168fd29fc62bc7
Paul <easylive1989@gmail.com>
2022-01-10 23:57:32 +0800

    remove unused dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:little_flower_app/service/ki_board_service.dart';
@@ -9,10 +8,8 @@ import 'main_app.dart';
 void main() async {
   configureInjection();
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   var kiBoardManager = GetIt.I<KiBoardService>();
   runApp(
     MainApp(kiBoardManager),
   );
 }
-
