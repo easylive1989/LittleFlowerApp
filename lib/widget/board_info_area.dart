d4f0b59c8b34c0bb36d13bbb4aedd0060c57ef7e
Paul <easylive1989@gmail.com>
2022-01-14 00:31:54 +0800

    remove unused ui

--- lib/widget/board_info_area.dart
+++ lib/widget/board_info_area.dart
@@ -1,22 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:little_flower_app/widget/board_id_widget.dart';
-import 'package:little_flower_app/widget/board_public_switch.dart';
 
 class BoardInfoArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.topCenter,
-      children: [
-        Align(
-          alignment: Alignment.topRight,
-          child: BoardPublicSwitch(),
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 80),
-          child: BoardIdWidget(),
-        ),
-      ],
+    return Padding(
+      padding: EdgeInsets.only(top: 80),
+      child: BoardIdWidget(),
     );
   }
 }
