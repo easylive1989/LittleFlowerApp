28fe34c82dc7da195f0c56a60aa221b8d756d3ad
Paul <easylive1989@gmail.com>
2022-01-15 19:41:48 +0800

    extract board selector

--- lib/widget/board_id_widget.dart
+++ lib/widget/board_id_widget.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:little_flower_app/controller/ki_board_controller.dart';
-import 'package:little_flower_app/service/ki_board_service.dart';
+import 'package:little_flower_app/widget/board_selector.dart';
 import 'package:little_flower_app/widget/delete_button.dart';
 import 'package:little_flower_app/widget/refresh_button.dart';
 import 'package:provider/provider.dart';
 
-class BoardIdWidget extends StatefulWidget {
+class BoardIdWidget extends StatelessWidget {
   const BoardIdWidget({Key? key}) : super(key: key);
-
-  @override
-  _BoardIdWidgetState createState() => _BoardIdWidgetState();
-}
-
-class _BoardIdWidgetState extends State<BoardIdWidget> {
-  bool _isListOpen = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +17,11 @@ class _BoardIdWidgetState extends State<BoardIdWidget> {
       children: [
         Container(
           width: 150,
-          height: _isListOpen ? 352 : 52,
           decoration: BoxDecoration(
             border: Border.all(color: Colors.black),
             borderRadius: BorderRadius.circular(5.0),
           ),
-          child: Column(
-            children: [
-              _buildBoardId(context, kiBoardController.boardId),
-              _isListOpen
-                  ? _buildBoardIdList(context, kiBoardController.allBoardId)
-                  : Container(),
-            ],
-          ),
+          child: _buildBoardSelector(context, kiBoardController.allBoardId),
         ),
         SizedBox(width: 10),
         RefreshButton(onTap: () {
@@ -51,83 +36,20 @@ class _BoardIdWidgetState extends State<BoardIdWidget> {
     );
   }
 
-  Widget _buildBoardIdList(
-      BuildContext context, Future<List<String>> getBoardIdFuture) {
+  Widget _buildBoardSelector(
+    BuildContext context,
+    Future<List<String>> getBoardIdFuture,
+  ) {
     return FutureBuilder<List<String>>(
-        future: getBoardIdFuture,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var allBoardIds = snapshot.data!;
-            return Container(
-              height: 300,
-              color: Colors.white,
-              child: ListView.builder(
-                padding: EdgeInsets.all(0),
-                itemCount: allBoardIds.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _isListOpen = false;
-                      });
-                      context
-                          .read<KiBoardController>()
-                          .changeKiBoard(allBoardIds[index]);
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 2),
-                      child: Text(
-                        allBoardIds[index],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            );
-          } else {
-            return Container();
-          }
-        });
-  }
-
-  Widget _buildBoardId(BuildContext context, String boardId) {
-    return Container(
-      height: 50,
-      child: Stack(
-        children: [
-          TextFormField(
-            key: ValueKey(boardId),
-            decoration: InputDecoration(border: InputBorder.none),
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20),
-            initialValue: boardId,
-            onTap: () {
-              setState(() {
-                _isListOpen = false;
-              });
-            },
-            onFieldSubmitted: (text) async =>
-                await context.read<KiBoardService>().changeKiBoard(text),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _isListOpen = !_isListOpen;
-                  FocusScope.of(context).requestFocus(new FocusNode());
-                });
-              },
-              child: Icon(
-                Icons.arrow_drop_down_rounded,
-                size: 40,
-              ),
-            ),
-          ),
-        ],
-      ),
+      future: getBoardIdFuture,
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          var allBoardIds = snapshot.data!;
+          return BoardSelector(allBoardIds: allBoardIds);
+        } else {
+          return Container();
+        }
+      },
     );
   }
 }
---
+++ lib/widget/board_selector.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:little_flower_app/controller/ki_board_controller.dart';
+import 'package:provider/provider.dart';
+
+class BoardSelector extends StatelessWidget {
+  const BoardSelector({
+    Key? key,
+    required this.allBoardIds,
+  }) : super(key: key);
+
+  final List<String> allBoardIds;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+        isExpanded: true,
+        onChanged: (value) {
+          context.read<KiBoardController>().changeKiBoard(value!);
+        },
+        value: context.read<KiBoardController>().boardId,
+        items: allBoardIds
+            .map((boardId) => DropdownMenuItem(
+                value: boardId,
+                child: Text(
+                  boardId,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                )))
+            .toList());
+  }
+}
