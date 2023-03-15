0bd248db7ca2911a456a248bda29d62d0c11d4b6
Paul <easylive1989@gmail.com>
2023-03-15 23:45:01 +0800

    fix web hit test

--- lib/widget/ki_board_area.dart
+++ lib/widget/ki_board_area.dart
@@ -6,21 +6,35 @@ import 'package:little_flower_app/model/ki_board.dart';
 import 'package:little_flower_app/widget/ki_board_painter.dart';
 import 'package:provider/provider.dart';
 
-class KiBoardArea extends StatelessWidget {
+class KiBoardArea extends StatefulWidget {
+  @override
+  State<KiBoardArea> createState() => _KiBoardAreaState();
+}
+
+class _KiBoardAreaState extends State<KiBoardArea> {
+  bool _pressed = false;
+
   @override
   Widget build(BuildContext context) {
     var controller = context.watch<KiBoardController>();
     return Center(
-      child: CustomPaint(
-        size: Size(300, 300),
-        painter: KiBoardPainter(
-          row: KiBoard.row,
-          column: KiBoard.column,
-          blackKiList: controller.board.blackKiList,
-          whiteKiList: controller.board.whiteKiList,
-          onTap: (x, y) {
-            context.read<KiBoardController>().addKi(Point(x, y));
-          },
+      child: GestureDetector(
+        onTapDown: (_) => setState(() => _pressed = true),
+        onTapUp: (_) => setState(() => _pressed = false),
+        child: CustomPaint(
+          size: Size(300, 300),
+          painter: KiBoardPainter(
+            row: KiBoard.row,
+            column: KiBoard.column,
+            blackKiList: controller.board.blackKiList,
+            whiteKiList: controller.board.whiteKiList,
+            onTap: _pressed
+                ? (x, y) {
+                    context.read<KiBoardController>().addKi(Point(x, y));
+                    setState(() => _pressed = false);
+                  }
+                : null,
+          ),
         ),
       ),
     );
